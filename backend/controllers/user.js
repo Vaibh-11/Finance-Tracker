0aa7569adfebfe183f4be7fbc2941e1fa2458424
vaibhav <solankivaibhav589@gmail.com>
2026-04-05 20:40:57 +0530

    changes

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -213,7 +213,7 @@ module.exports.forgotPassword = async (req, res) => {
 
     transporter
       .sendMail({
-        from: process.env.EMAIL_USER,
+        from: "test@mailtrap.io",
         to: email,
         subject: "Password Reset OTP",
         text: `Your OTP is ${otp}`,
