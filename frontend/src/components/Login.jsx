69fdf12e2a9195afecd2417abf5efa94e1bb8aed
vaibhav <solankivaibhav589@gmail.com>
2026-04-05 23:56:16 +0530

    chnages

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { clearUser } from "../redux/authSlice";
+import { clearUser } from "../utils/authSlice";
 import api from "../utils/axios";
 
 const Navbar = () => {
