62478f75bc433c106cb078e98264628d4661ce7b
vaibhav <solankivaibhav589@gmail.com>
2026-04-05 21:01:03 +0530

    chnage

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -206,7 +206,8 @@ module.exports.forgotPassword = async (req, res) => {
 
     const transporter = nodemailer.createTransport({
       host: "sandbox.smtp.mailtrap.io",
-      port: 2525,
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
