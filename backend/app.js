c620d3a23e3f5429f3494d3652a1e4b1ba8d74c1
vaibhav <solankivaibhav589@gmail.com>
2026-04-05 19:48:20 +0530

    updatedApp

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const connectDB = require("./config/database");
@@ -5,18 +7,16 @@ const userRouter = require("./routes/userRouter");
 const transactionRouter = require("./routes/transactionRouter");
 const cookie = require("cookie-parser");
 const cors = require("cors");
-require("dotenv").config();
 
 const allowedOrigins = [
   "https://finance-tracker-lemon-eta.vercel.app",
-  "http://localhost:4000",
+  "http://localhost:3000",
 ];
 
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (!origin) return callback(null, true); // allow Postman
-
+      if (!origin) return callback(null, true);
       if (allowedOrigins.includes(origin)) {
         return callback(null, true);
       } else {
@@ -29,22 +29,20 @@ app.use(
 
 app.use(express.json());
 app.use(cookie());
-app.use("/user", userRouter);
-app.use("/transaction", transactionRouter);
-
-connectDB()
-  .then(() => {
-    console.log("Database Connected Succesfully");
-    const PORT = process.env.PORT || 4000;
-
-    app.listen(PORT, () => {
-      console.log("Server Start Successfully on", PORT);
-    });
-  })
-  .catch((err) => {
-    console.log("Error Successfully");
-  });
 
 app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
 });
+
+app.use("/user", userRouter);
+app.use("/transaction", transactionRouter);
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log("Server Start Successfully on", PORT);
+});
+
+connectDB()
+  .then(() => console.log("Database Connected Successfully"))
+  .catch((err) => console.log("DB Error:", err));
